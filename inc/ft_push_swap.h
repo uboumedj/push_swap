f0c9796fe7686233e3b66b1a553950e397f9f148
Unknown <uboumedj@student.42.fr>
2018-01-12 21:40:54 +0100

    starting project

--- inc/ft_push_swap.h
+++ inc/ft_push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_push_swap.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: uboumedj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: uboumedj <uboumedj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/12 14:43:43 by uboumedj          #+#    #+#             */
-/*   Updated: 2018/01/12 16:15:25 by uboumedj         ###   ########.fr       */
+/*   Updated: 2018/01/12 21:40:22 by uboumedj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,9 @@ void				pb(t_pile *a, t_pile *b);
 void				ra(t_pile *a);
 void				rb(t_pile *b);
 void				rr(t_pile *a, t_pile *b);
+
+void				rra(t_pile *a);
+void				rrb(t_pile *b);
+void				rrr(t_pile *a, t_pile *b);
 
 #endif
--- srcs/ft_revrotate.c
+++ srcs/ft_revrotate.c
@@ -3,29 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   ft_revrotate.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: uboumedj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: uboumedj <uboumedj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/12 16:17:51 by uboumedj          #+#    #+#             */
-/*   Updated: 2018/01/12 16:29:37 by uboumedj         ###   ########.fr       */
+/*   Updated: 2018/01/12 21:40:22 by uboumedj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ft_push_swap.h"
 
-void	revrotate(t_pile *pile)
+static void		revrotate(t_pile *pile)
 {
-	t_list	*last;
-	int		temp;
+	t_list	*inc;
+	int		prev_c;
+	int		cur_c;
 
 	if (pile && pile->next)
 	{
-		last = pile;
-		temp = last->content;
-		while (last->next)
+		inc = pile;
+		prev_c = inc->content;
+		while (inc->next)
 		{
-			temp = last->next->content;
-			last->next->content = last->content;
-			last = last->next;
+			cur_c = inc->next->content;
+			inc->next->content = prev_c;
+			prev_c = cur_c;
+			inc = inc->next;
 		}
+		pile->content = prev_c;
 	}
+}
+
+void					rra(t_pile *a)
+{
+	revrotate(a);
+}
+
+void					rrb(t_pile *b)
+{
+	revrotate(b);
+}
+
+void					rrr(t_pile *a, t_pile *b)
+{
+	revrotate(a);
+	revrotate(b);
 }
